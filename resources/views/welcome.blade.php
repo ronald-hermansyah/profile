798b089ba4adc1c2183fa43cc1d0739042f4cdef
r1345678 <142782406+r1345678@users.noreply.github.com>
2023-08-28 13:54:37 +0700

    profile

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <section class="vh-100" style="background-color: black;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-md-9 col-lg-7 col-xl-5">
+            <div class="card" style="border-radius: 15px;">
+              <div class="card-body p-4">
+                <div class="d-flex text-black">
+                  <div class="flex-shrink-0">
+                    <img src="c:\Users\lab1-komputer18\Downloads\rzx.jpg"
+                      alt="Generic placeholder image" class="img-fluid"
+                      style="width: 180px; border-radius: 10px;">
+                  </div>
+                  <div class="flex-grow-1 ms-3">
+                    <h5 class="mb-1">Ronald Hermansyah</h5>
+                    <p class="mb-2 pb-1" style="color: #2b2a2a;">Atlit Pencak Silat</p>
+                    <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                      style="background-color: #efefef;">
+                      <div>
+                        <p class="small text-muted mb-1">Age</p>
+                        <p class="mb-0">17</p>
+                      </div>
+                      <div class="px-3">
+                        <p class="small text-muted mb-1">Height</p>
+                        <p class="mb-0">171</p>
+                      </div>
+                      <div>
+                        <p class="small text-muted mb-1">Weight</p>
+                        <p class="mb-0">47.9</p>
+                      </div>
+                    </div>
+                    <div class="d-flex pt-1">
+                      <button type="button" class="btn btn-outline-primary me-1 flex-grow-1">Chat</button>
+                      <button type="button" class="btn btn-primary flex-grow-1">Follow</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
